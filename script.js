5901cd6345e8b7e4c151c68547e41fcc428975e7
Quentin <quentin.software@gmail.com>
2024-07-09 22:49:30 -0400

    Added size conversions, commented out unused code

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 //alert("Javascript has been added");
+/*
 
 function addition(p1, p2){
     return p1 + p2;
+}
+
+*/
+
+
+function milesToKilometers(){
+    var miles = document.getElementById("milesToKilometers").value;
+    var milesFloat = parseFloat(miles);
+
+    document.getElementById("mToK").innerHTML = milesFloat * 1.60934;
+}
+
+function kilometersToMiles(){
+    var kilometers = document.getElementById("kilometersToMiles").value;
+    var kilometersFloat = parseFloat(kilometers);
+
+    document.getElementById("kToM").innerHTML = kilometersFloat * 0.621371;
 }
